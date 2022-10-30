8fb3c1b16249c175381d27fc3a2a3ff044a2a5e2
@anujina402 <anujina402@gmail.com>
2022-10-30 20:06:25 +0800

    k

---
+++ components/Card.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { withNavigation } from '@react-navigation/compat';
+import PropTypes from 'prop-types';
+import { StyleSheet, Dimensions, Image, TouchableWithoutFeedback } from 'react-native';
+import { Block, Text, theme } from 'galio-framework';
+
+import { argonTheme } from '../constants';
+
+
+class Card extends React.Component {
+  render() {
+    const { navigation, item, horizontal, full, style, ctaColor, imageStyle } = this.props;
+    
+ 
+    const cardContainer = [styles.card, styles.shadow, style];
+ 
+
+    return (
+      <Block row={horizontal} card flex style={cardContainer}>
+       
+        <TouchableWithoutFeedback onPress={() => navigation.navigate('Pro')}>
+          <Block flex space="between" style={styles.cardDescription}>
+            <Text size={15} style={styles.cardTitle}>{item.title}</Text>
+            <Text size={11} muted={!ctaColor} color={ctaColor || argonTheme.COLORS.ACTIVE} bold>{item.cta}</Text>
+          </Block>
+        </TouchableWithoutFeedback>
+      </Block>
+    );
+  }
+}
+
+Card.propTypes = {
+  item: PropTypes.object,
+  horizontal: PropTypes.bool,
+  full: PropTypes.bool,
+  ctaColor: PropTypes.string,
+  imageStyle: PropTypes.any,
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: theme.COLORS.WHITE,
+    marginVertical: theme.SIZES.BASE,
+    borderWidth: 0,
+    minHeight: 30,
+    marginBottom: 1
+  },
+  cardTitle: {
+    flex: 1,
+    flexWrap: 'wrap',
+    paddingBottom: 4
+  },
+  cardDescription: {
+    padding: theme.SIZES.BASE / 1
+  },
+  imageContainer: {
+    borderRadius: 3,
+    elevation: 1,
+    overflow: 'hidden',
+  },
+  image: {
+    // borderRadius: 3,
+  },
+ 
+  horizontalStyles: {
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+  },
+  verticalStyles: {
+    borderBottomRightRadius: 0,
+    borderBottomLeftRadius: 0
+  },
+  fullImage: {
+    height: 215
+  },
+  shadow: {
+    shadowColor: theme.COLORS.BLACK,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 4,
+    shadowOpacity: 0.1,
+    elevation: 2,
+  },
+});
+
+export default withNavigation(Card);
---
+++ constants/articles.js
@@ -0,0 +1,87 @@
+export default [
+  {
+    title: 'HTML гэж юу вэ…',
+    image: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+    cta: 'Дэлгэрэнгүй', 
+    horizontal: true
+  },
+  {
+    title: 'HTML элемэнтүүд …',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    cta: 'Дэлгэрэнгүй'
+  },
+  {
+    title: 'HTML атрибутууд болон HTML ШИНЖ ЧАНАРУУД …',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    cta: 'Дэлгэрэнгүй' 
+  },
+  {
+    title: 'HTML ГАРЧИГУУД …',
+    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    cta: 'Дэлгэрэнгүй' 
+  },
+  {
+    title: 'HTML-ИЙН ДОГОЛ МӨРҮҮД(PARAGRAPHS)…',
+    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    cta: 'Дэлгэрэнгүй', 
+    horizontal: true
+  },
+  {
+    title: 'HTML ӨНГҮҮД',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    cta: 'Дэлгэрэнгүй'
+  },
+  {
+    title: 'HTML ХОЛБООСУУД',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    cta: 'Дэлгэрэнгүй' 
+  },
+  {
+    title: 'HTML TABLES',
+    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    cta: 'Дэлгэрэнгүй' 
+  },
+  {
+    title: 'HTML STYLES',
+    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    cta: 'Дэлгэрэнгүй', 
+    horizontal: true
+  },
+  {
+    title: 'HTML ЖАГСААЛТУУД',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    cta: 'Дэлгэрэнгүй'
+  },
+  {
+    title: 'HTML JAVASCRIPT',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    cta: 'Дэлгэрэнгүй' 
+  },
+  {
+    title: 'HTML RESPONSIVE WEB DESIGN',
+    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    cta: 'Дэлгэрэнгүй' 
+  },
+  {
+    title: 'HTML ТЕКСТ ХЭЛБЭРЖҮҮЛЭХ',
+    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    cta: 'Дэлгэрэнгүй', 
+    horizontal: true
+  },
+  {
+    title: 'HTML FORMS',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    cta: 'Дэлгэрэнгүй'
+  },
+  {
+    title: 'HTML MEDIA',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    cta: 'Дэлгэрэнгүй' 
+  },
+  {
+    title: 'HTML-ИЙН ЭХ АВАХ БОЛОН ИШ ТАТАХ ЭЛЕМЕНТҮҮД',
+    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    cta: 'Дэлгэрэнгүй', 
+    horizontal: true
+  },
+];
